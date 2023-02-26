dfadbb9975c29cee2f5c652c835e5b686511f17c
alfre <alfredo.medina@uc.cl>
2023-02-26 03:08:37 -0300

    ft: favicon

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -17,6 +17,6 @@ const Container = styled.div`
     align-items: center;
     font-family: "Century Gothic",Verdana,sans-serif;
     font-size: 3em;
-    color: #3730a3;
+    color: #6366f1;
     margin: 40px;
 `;
