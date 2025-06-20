3827a1eef7a75ec786e97e4ea03cf4ea03233554
AlfreMK <alfredo.medina@thenotcompany.com>
2025-06-19 22:15:58 -0400

    chore: react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import Footer from './components/Footer.tsx'
+import '@/index.css'
+import App from '@/App.tsx'
+import Footer from '@/components/Footer.tsx'
+import Logo from '@/components/Logo.tsx'
+import { Routes, Route, BrowserRouter} from 'react-router'
+
+const BASE_URL = "watchwhere"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Logo />
+    <BrowserRouter>
+    <Routes>
+      <Route path={`${BASE_URL}/`} element={<App />} />
+      <Route path={`${BASE_URL}/:media/:id`} element={<></>}/>
+    </Routes>
+    </BrowserRouter>
     <Footer />
   </StrictMode>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
